2123bef6c5018f4f70225ef0c082b54556d35c05
Timothy Pakkianathan <ptimothyp@gmail.com>
2010-02-24 21:13:18 +1100

    Removed the unwanted where function

--- simple-database.lisp
+++ simple-database.lisp
@@ -43,15 +43,6 @@
     (with-standard-io-syntax
       (setf *db* (read in)))))
 
-
-(defun where (&key title artist rating (ripped nil ripped-p))
-  #'(lambda (cd)
-      (and
-       (if title (equal (getf cd :title) title) t)
-       (if artist (equal (getf cd :artist) artist) t)
-       (if rating (equal (getf cd :rating) rating) t)
-       (if ripped-p (equal (getf cd :ripped) ripped) t))))
-
 (defun select (selector-fn)
   (remove-if-not selector-fn *db*))
 
